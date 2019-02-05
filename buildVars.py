4d00d22efd50279f851a51210fc8b350d1a63082
Jose Manuel Delicado <jmdaweb@hotmail.com>
2019-02-05 13:17:21 +0100

    Set update channel to None. @josephsl

--- buildVars.py
+++ buildVars.py
@@ -27,7 +27,7 @@ This gesture can be changed from Input Gestures dialog."""),
 	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
 	"addon_lastTestedNVDAVersion" : "2019.1.0",
 	# Add-on update channel (default is stable or None)
-	"addon_updateChannel" : "stable",
+	"addon_updateChannel" : None,
 }
 
 
